e1ce289201c8d2b58aa656083bc075c39480910f
Darshana <keerthi.mac@gmail.com>
2022-07-03 22:33:58 +0530

    login register home ui finished

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,64 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+
 function Login() {
-  return <div>Login</div>;
+  const [fromData, setFromData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = fromData;
+
+  const onChange = (e) => {
+    setFromData({
+      ...fromData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(fromData);
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              name='email'
+              className='form-control'
+              id='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your Email'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              name='password'
+              className='form-control'
+              id='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter Password'
+            />
+          </div>
+
+          <button className='btn btn-block'>Submit</button>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,82 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+
 function Register() {
-  return <div>Register</div>;
+  const [fromData, setFromData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { name, email, password, confirmPassword } = fromData;
+
+  const onChange = (e) => {
+    setFromData({
+      ...fromData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className='form'>
+        <form>
+          <div className='form-group'>
+            <input
+              type='text'
+              name='name'
+              className='form-control'
+              id='name'
+              value={name}
+              onChange={onChange}
+              placeholder='Enter your name'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              name='email'
+              className='form-control'
+              id='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your Email'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              name='password'
+              className='form-control'
+              id='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter Password'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              name='confirmPassword'
+              className='form-control'
+              id='confirmPassword'
+              value={confirmPassword}
+              onChange={onChange}
+              placeholder='confirm Password'
+            />
+          </div>
+          <button className='btn btn-block'>Submit</button>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Register;
