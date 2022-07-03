3b7ff83e48409a9b34bb3a246d036d50a226b603
Darshana <keerthi.mac@gmail.com>
2022-07-03 20:53:15 +0530

    header and home

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div></div>;
+  return (
+    <>
+      <header className='header'>
+        <Link to='/' className='logo'>
+          Support Desk
+        </Link>
+        <ul>
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/register'>Register</Link>
+            </li>
+          </>
+        </ul>
+      </header>
+    </>
+  );
 }
 
 export default Header;
