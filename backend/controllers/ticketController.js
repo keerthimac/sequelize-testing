53e60e75cc4f9cb49753acd81cffd15d6aeef3b1
Darshana <keerthi.mac@gmail.com>
2022-07-10 13:51:12 +0530

    ticket item on dev

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -16,7 +16,7 @@ const getTickets = asyncHandler(async (req, res) => {
   }
 
   const tickets = await Ticket.findAll({
-    attributes: ["product", "description"],
+    attributes: ["id", "product", "description", "createdAt"],
     include: {
       model: User,
       where: { Id: id },
--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -4,31 +4,51 @@ import { toast } from "react-toastify";
 import { getTickets } from "../features/ticket/ticketSlice";
 import { reset } from "../features/ticket/ticketSlice";
 import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 
 function Tickets() {
-  const dispatch = useDispatch();
   const { tickets, isError, isLoading, isSuccess, message } = useSelector(
     (state) => state.ticket
   );
+
+  console.log(tickets);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (isSuccess) {
       toast.success(message);
       dispatch(reset());
     }
-  }, [dispatch]);
+  }, [dispatch, isSuccess]);
 
   useEffect(() => {
     dispatch(getTickets());
-    if (isError) {
-      toast.error(message);
-    }
   }, [dispatch]);
 
   if (isLoading) {
     <Spinner />;
   }
-  return <h1>Tickets</h1>;
+
+  return (
+    <>
+      <BackButton url='/' />
+      <h1>Tickets</h1>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {/* <TicketItem key={tickets[1].id} ticket={tickets[1]} />; */}
+        {tickets.map((ticket) => {
+          <TicketItem key={ticket.id} ticket={ticket} />;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Tickets;
