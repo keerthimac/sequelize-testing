48fb92eacc3b6f40a2ba961a092dc23a341ea0bc
Darshana <keerthi.mac@gmail.com>
2022-07-10 19:57:52 +0530

    jwt set to 1d

--- backend/utils/jwtGenerator.js
+++ backend/utils/jwtGenerator.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 const jwtGenerator = (id) => {
   const jwtSecret = process.env.JWT_SECRET;
   const payload = { id };
-  const token = jwt.sign(payload, jwtSecret, { expiresIn: "1h" });
+  const token = jwt.sign(payload, jwtSecret, { expiresIn: "1d" });
   return token;
 };
 
